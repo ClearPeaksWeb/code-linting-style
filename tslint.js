39033ad08b79c13174fef92e8810625e7788eb54
ignacioPastor <ignacio.pastor@clearpeaks.com>
2018-04-10 09:41:05 +0200

    updated rules

--- tslint.js
+++ tslint.js
@@ -6,80 +6,99 @@ module.exports = {
     path.join(path.dirname(require.resolve('tslint-eslint-rules')), 'dist/rules'),
     path.join(path.dirname(require.resolve('tslint-microsoft-contrib')), './'),
   ],
-  rules: { // base on https://github.com/airbnb/javascript/tree/44dbd0bdc41d08eb5de8ad698099ae44240f4b0d
-    'prefer-const': true, // 2.1, 13.1
-    'no-var-keyword': true, // 2.2
-    // 'object-literal-shorthand': true, // 3.3, 3.4
-    // 'object-shorthand-properties-first': true, // 3.5
-    // 'object-literal-key-quotes': [true, 'as-needed'], // 3.6
-    // 'prefer-array-literal': true, // 4.1
+  rules: {
+    'prefer-const': true,
+    'no-var-keyword': true,
     quotemark: [
       true,
       'single',
       'jsx-double',
-    ], // 6.1
-    'no-eval': true, // 6.4
-    'no-function-constructor-with-string-args': true, // 7.10
+    ],
+    'no-eval': true,
+    'no-function-constructor-with-string-args': true,
     'space-before-function-paren': [
       true,
       {
-        anonymous: 'always', // 7.11
-        named: 'never', // 19.3
+        anonymous: 'always',
+        named: 'never',
       },
     ],
-    // align: [
-    //   true,
-    //   'arguments',
-    //   'parameters',
-    // ], // 7.15
-    'ter-prefer-arrow-callback': [true], // 8.1
-    'arrow-parens': false, // 8.2
+    "align": [
+      true,
+      "arguments",
+      "parameters",
+      "statements"
+    ],
+    "interface-name": [
+      true,
+      "always-prefix"
+    ],
+    "interface-over-type-literal": true,
+    "ban-types": [
+      true, [
+        "Array",
+        "Avoid using the `Array` type. Did you mean `[]`?"
+      ],
+      [
+        "Object",
+        "Avoid using the `Object` type. Did you mean `{}`?"
+      ],
+      [
+        "String",
+        "Avoid using the `String` type. Did you mean `string`?"
+      ],
+      [
+        "Boolean",
+        "Avoid using the `Boolean` type. Did you mean `boolean`?"
+      ]
+    ],
+    "no-arg": true,
+    'ter-prefer-arrow-callback': [true],
+    'arrow-parens': false,
     'ter-arrow-parens': [
       true,
       'as-needed',
       { 'requireForBlockBody': true },
-    ], // 8.4
-    'no-duplicate-imports': true, // 10.4
-    'one-variable-per-declaration': [true, 'ignore-for-loop'], // 13.2
-    // 'no-increment-decrement': true, // 13.6
-    'triple-equals': [true, 'allow-null-check'], // 15.1
-    'no-boolean-literal-compare': true, // 15.3
-    curly: [true, 'ignore-same-line'], // 16.1
+    ],
+    'no-duplicate-imports': true,
+    'one-variable-per-declaration': [true, 'ignore-for-loop'],
+    'triple-equals': [true, 'allow-null-check'],
+    'no-boolean-literal-compare': true,
+    curly: [true, 'ignore-same-line'],
     'brace-style': [
       true,
       '1tbs',
       { allowSingleLine: true },
-    ], // 16.2
-    'no-else-after-return': true, // 16.3
-    'comment-format': [true, 'check-space'], // 18.3
-    indent: [true, 'space'], // 19.1
+    ],
+    'no-else-after-return': true,
+    'comment-format': [true, 'check-space'],
+    indent: [true, 'space'],
     'ter-indent': [
       true,
       2,
       { 'SwitchCase': 1 },
-    ], // 19.1
+    ],
     whitespace: [
       true,
-      'check-branch', // 19.3
-      'check-decl', // 19.4
-      'check-operator', // 19.4
-      'check-preblock', // 19.2
+      'check-branch',
+      'check-decl',
+      'check-operator',
+      'check-preblock',
     ],
-    eofline: true, // 19.5
-    'space-in-parens': [true, 'never'], // 19.9
-    'array-bracket-spacing': [true, 'never'], // 19.10
-    'object-curly-spacing': [true, 'always'], // 19.11
-    'max-line-length': [true, 120], // 19.12
+    eofline: true,
+    'space-in-parens': [true, 'never'],
+    'array-bracket-spacing': [true, 'never'],
+    'object-curly-spacing': [true, 'always'],
+    'max-line-length': [true, 120],
     'trailing-comma': [
       true,
       {
         multiline: 'never',
         singleline: 'never',
       },
-    ], // 20.2
-    semicolon: [true, 'always'], // 21.1
-    'no-construct': true, // 22.2, 22.3, 22.6
-    // radix: true, // 22.3
+    ],
+    semicolon: [true, 'always'],
+    'no-construct': true,
     'function-name': [
       true,
       {
@@ -88,10 +107,26 @@ module.exports = {
         'private-method-regex': /^[a-z$][\w\d]+$/,
         'protected-method-regex': /^[a-z$][\w\d]+$/,
         'static-method-regex': /^[a-z$][\w\d]+$/,
-      }, // 23.1
+      },
     ],
-    // 'variable-name': [true, 'check-format'], // 23.2  // this would be great but we depend many times on backend variables
-    // 'no-this-assignment': true, // 23.5
-    'import-name': true, // 23.6
+    'import-name': true,
+    "no-consecutive-blank-lines": [
+      true,
+      1
+    ],
+    "no-console": [
+      true,
+      "debug",
+      "info",
+      "log",
+      "time",
+      "timeEnd",
+      "trace"
+    ],
+    "no-debugger": true,
+    "no-duplicate-variable": true,
+    "no-unused-variable": [
+      true
+    ],
   },
 };
