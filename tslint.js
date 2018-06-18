6ee429f2342631ad9189b8078f9ecadf57d3ca1d
ignacioPastor <ignacio.pastor@clearpeaks.com>
2018-06-18 13:02:33 +0200

    allow semicolon in boud methods to avoid conflicts with prettify

--- tslint.js
+++ tslint.js
@@ -2,57 +2,41 @@ const path = require("path");
 
 module.exports = {
   rulesDirectory: [
-    path.join(path.dirname(require.resolve("tslint-consistent-codestyle")), "./"),
-    path.join(path.dirname(require.resolve("tslint-eslint-rules")), "dist/rules"),
-    path.join(path.dirname(require.resolve("tslint-microsoft-contrib")), "./"),
+    path.join(
+      path.dirname(require.resolve("tslint-consistent-codestyle")),
+      "./"
+    ),
+    path.join(
+      path.dirname(require.resolve("tslint-eslint-rules")),
+      "dist/rules"
+    ),
+    path.join(path.dirname(require.resolve("tslint-microsoft-contrib")), "./")
   ],
   rules: {
     "prefer-const": true,
     "no-var-keyword": true,
-    quotemark: [
-      true,
-      "single",
-      "jsx-double",
-    ],
+    quotemark: [true, "single", "jsx-double"],
     "no-eval": true,
     "no-function-constructor-with-string-args": true,
     "space-before-function-paren": [
       true,
       {
-        "anonymous": "never",
-        "named": "never",
-        "asyncArrow": "always",
-        "method": "never",
-        "constructor": "never"
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "always",
+        method: "never",
+        constructor: "never"
       }
     ],
-    "align": [
-      true,
-      "parameters",
-      "statements"
-    ],
-    "interface-name": [
-      true,
-      "always-prefix"
-    ],
+    align: [true, "parameters", "statements"],
+    "interface-name": [true, "always-prefix"],
     "interface-over-type-literal": true,
     "ban-types": [
-      true, [
-        "Array",
-        "Avoid using the `Array` type. Did you mean `[]`?"
-      ],
-      [
-        "Object",
-        "Avoid using the `Object` type. Did you mean `{}`?"
-      ],
-      [
-        "String",
-        "Avoid using the `String` type. Did you mean `string`?"
-      ],
-      [
-        "Boolean",
-        "Avoid using the `Boolean` type. Did you mean `boolean`?"
-      ]
+      true,
+      ["Array", "Avoid using the `Array` type. Did you mean `[]`?"],
+      ["Object", "Avoid using the `Object` type. Did you mean `{}`?"],
+      ["String", "Avoid using the `String` type. Did you mean `string`?"],
+      ["Boolean", "Avoid using the `Boolean` type. Did you mean `boolean`?"]
     ],
     "no-arg": true,
     "ter-prefer-arrow-callback": [true],
@@ -60,23 +44,19 @@ module.exports = {
     "no-duplicate-imports": true,
     "one-variable-per-declaration": [true, "ignore-for-loop"],
     "triple-equals": [true, "allow-null-check"],
-    "curly": [true, "ignore-same-line"],
-    "brace-style": [
-      true,
-      "1tbs",
-      { "allowSingleLine": true },
-    ],
+    curly: [true, "ignore-same-line"],
+    "brace-style": [true, "1tbs", { allowSingleLine: true }],
     "no-else-after-return": true,
     "comment-format": [true, "check-space"],
-    "indent": [true, "spaces", 2],
-    "whitespace": [
+    indent: [true, "spaces", 2],
+    whitespace: [
       true,
       "check-branch",
       "check-decl",
       "check-operator",
-      "check-preblock",
+      "check-preblock"
     ],
-    "eofline": true,
+    eofline: true,
     "space-in-parens": [true, "never"],
     "array-bracket-spacing": [true, "never"],
     "object-curly-spacing": [true, "always"],
@@ -85,10 +65,10 @@ module.exports = {
       true,
       {
         multiline: "never",
-        singleline: "never",
-      },
+        singleline: "never"
+      }
     ],
-    semicolon: [true, "always"],
+    semicolon: [true, "always", "ignore-bound-class-methods"],
     "no-construct": true,
     "function-name": [
       true,
@@ -97,21 +77,13 @@ module.exports = {
         "method-regex": /^[a-z$][\w\d]+$/,
         "private-method-regex": /^[a-z$][\w\d]+$/,
         "protected-method-regex": /^[a-z$][\w\d]+$/,
-        "static-method-regex": /^[a-z$][\w\d]+$/,
-      },
+        "static-method-regex": /^[a-z$][\w\d]+$/
+      }
     ],
     "import-name": true,
     "no-consecutive-blank-lines": [true, 1],
-    "no-console": [
-      true,
-      "debug",
-      "info",
-      "log",
-      "time",
-      "timeEnd",
-      "trace"
-    ],
+    "no-console": [true, "debug", "info", "log", "time", "timeEnd", "trace"],
     "no-debugger": true,
     "no-duplicate-variable": true
-  },
+  }
 };
